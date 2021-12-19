15afa150af71a975efcadce4f2af1b4d75597f58
Daniel Kogan <54873722+daminals@users.noreply.github.com>
2021-12-19 18:38:31 -0500

    works works works (video generation)

--- react/src/FormRequest.js
+++ react/src/FormRequest.js
@@ -1,11 +1,34 @@
-import React from 'react'
+import React, { useState, useRef, useEffect } from 'react';
+import axios from 'axios';
+import Cookie from 'universal-cookie';
 
 var sorryList = ["stealing candy from a baby","eating bacon on shabbat", "causing the cuban missle crisis"];
 export default function FormRequest() {
+    const cookies = new Cookie();
+    const sorRef = useRef();
+
+    function cookieID() {
+        var ct = new Date();
+        var [year,month,day,hour,min,sec] = [ct.getFullYear(),ct.getMonth(),ct.getDay(),ct.getHours(),ct.getMinutes(),ct.getSeconds()];
+        var now = year.toString() + month.toString() + day.toString() + hour.toString() + min.toString() + sec.toString();
+        console.log(now)
+        return now
+      };
+
+    function pRqst(e){
+        e.preventDefault();
+        const name = sorRef.current.value
+        if (name === '') return
+        //axios.post("/generator/"+{name}+"/start");
+        sorRef.current.value = null;
+        var cookie_id = cookieID()
+        cookies.set(cookie_id, name, {path: '/'});
+    }
+
     return (
         <form>
-            <input class="reason formH" placeholder={sorryList[Math.floor(Math.random()*sorryList.length)]} ></input>
-            <input class="btn-success formH" type="submit" value="Build!"></input>
+            <input class="reason formH" ref={sorRef} type="text" placeholder={sorryList[Math.floor(Math.random()*sorryList.length)]} ></input>
+            <input class="btn-success formH" type="submit" value="Build!" onClick={pRqst}></input>
         </form>
     )
 }
