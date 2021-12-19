4ad29699dce56810717deb4da920e503406d7a29
Daniel Kogan <54873722+daminals@users.noreply.github.com>
2021-12-19 00:34:46 -0500

    split screen

---
+++ react/src/Left.js
@@ -0,0 +1,11 @@
+import React from 'react'
+import FormRequest from './FormRequest';
+
+export default function Left() {
+    return (
+        <div class="split left text-center">
+            <h1> What are you apologizing for?</h1>
+            <FormRequest></FormRequest>
+        </div>
+    )
+}
---
+++ react/src/Right.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export default function Right() {
+    return (
+        <div class="split right">
+            
+        </div>
+    )
+}
